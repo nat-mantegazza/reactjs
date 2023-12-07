eb9bbc42c7709e07011845240832b9582c1f01fa
nat-mantegazza <natt.mante@gmail.com>
2023-12-06 21:15:07 -0300

    navbar design

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,24 +1,59 @@
 import React from 'react'
 import CartWidget from './CartWidget'
-import { Flex, Spacer, Box } from '@chakra-ui/react'
+import { Flex, Spacer, Box, Text, Menu, MenuButton, MenuList, MenuItem, Button } from '@chakra-ui/react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faWineGlassEmpty } from '@fortawesome/free-solid-svg-icons'
+
 
 
 const NavBar = () => {
   return (
     <div>
-        <Flex>
-        <Box p='4' bg='red.400'>
-            <FontAwesomeIcon icon={faWineGlassEmpty} />
+        <Flex bg='red.50' alignItems="center">
+            <Box p='4' bg='red.600'>
+                <Text fontSize='lg'>
+                    <FontAwesomeIcon icon={faWineGlassEmpty} /> WineStore
+                </Text>
             
-            WineStore
-        </Box>
-        <Spacer />
+            </Box>
+            <Spacer />
 
-        <Box p='4' bg='green.400'>
-            <CartWidget />
-        </Box>
+            <Button colorScheme='red' variant='ghost'>
+                Inicio
+            </Button>
+            
+            <Spacer />
+
+            <Menu>
+                <MenuButton>
+                    Vinos
+                </MenuButton>
+                <MenuList>
+                    <MenuItem>Tinto</MenuItem>
+                    <MenuItem>Blanco</MenuItem>
+                    <MenuItem>Rosado</MenuItem>
+                </MenuList>
+            </Menu>
+            <Spacer />
+
+            <Menu>
+                <MenuButton>
+                    Accesorios
+                </MenuButton>
+                <MenuList>
+                    <MenuItem>Sacacorchos</MenuItem>
+                    <MenuItem>Posavasos</MenuItem>
+                    <MenuItem>Decantadores</MenuItem>
+                </MenuList>
+            </Menu>
+
+    
+            
+            <Spacer />
+
+            <Box p='4' bg='red.50'>
+                <CartWidget />
+            </Box>
         </Flex>
         
         
