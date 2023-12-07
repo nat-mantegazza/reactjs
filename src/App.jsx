340fe2d2394e51acae731445053fedb6ea01a971
nat-mantegazza <natt.mante@gmail.com>
2023-12-06 21:37:26 -0300

    greeting created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from './components/NavBar'
+import ItemListContainer from './components/ItemListContainer'
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <ItemListContainer greeting={"Bienvenidos a la tienda!"} />
     </div>
   )
 }
