dd2a24a4fd15de041938ce3af414702f8634bdfb
nat-mantegazza <natt.mante@gmail.com>
2023-12-27 03:48:56 -0300

    item detail creation

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,23 +1,32 @@
+
 import React from 'react'
 import { Card, Heading, Stack, Text, CardBody, Divider, CardFooter, Button, ButtonGroup } from '@chakra-ui/react'
+import ItemCount from './ItemCount'
 
-
-const Item = ({ titulo, descripcion, precio }) => {
+const ItemDetail = ({ titulo, descripcion, precio }) => {
   return (
     <Card maxW='sm'>
         <CardBody>
             <Stack mt='6' spacing='3'>
             <Heading size='md'>{titulo}</Heading>
+            <Text>
+                {descripcion}
+            </Text>
+            <Text color='red.600' fontSize='2xl'>
+                ${precio}
+            </Text>
             </Stack>
         </CardBody>
         <Divider />
         <CardFooter>
             <ButtonGroup spacing='2'>
-                <Button> Ver detalle </Button>
+                <ItemCount />
             </ButtonGroup>
         </CardFooter>
     </Card>
   )
 }
 
-export default Item
+
+
+export default ItemDetail
